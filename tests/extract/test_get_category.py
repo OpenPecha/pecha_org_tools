d0067eb55d483b6b21861e2bd23fbef0bfaec5b3
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-12-20 16:10:31 +0530

    add test cases to test get category by lang

--- tests/extract/test_get_category.py
+++ tests/extract/test_get_category.py
@@ -29,6 +29,68 @@ def test_get_category_by_lang():
         }
     ]
 
+    category_name = "ཀུན་བཟང་སྨོན་ལམ་རྩ་བ་དེབ་།"
+    output = categorizer.get_category_by_lang(category_name, "bo")
+    assert output == [
+        {
+            "name": "ཁ་འདོན།",
+            "heDesc": "ཁ་འདོན་འགྲེལ་བཤད་",
+            "heShortDesc": "ཁ་འདོན་འགྲེལ་བཤད་ཐུང་ཐུང་",
+        },
+        {
+            "name": "སྨོན་ལམ།",
+            "heDesc": "སྨོན་ལམ་འགྲེལ་བཤད་",
+            "heShortDesc": "སྨོན་ལམ་འགྲེལ་བཤད་ཐུང་ཐུང་",
+        },
+        {"name": "ཀུན་བཟང་སྨོན་ལམ།", "heDesc": "", "heShortDesc": ""},
+        {"name": "རྩ་བ།", "heDesc": "", "heShortDesc": ""},
+        {"name": "ཀུན་བཟང་སྨོན་ལམ་རྩ་བ་དེབ་།", "heDesc": "", "heShortDesc": ""},
+    ]
+
+    category_name = "བཟང་སྤྱོད་སྨོན་ལམ་འགྲེལ་བ་དེབ་། ༢"
+    output = categorizer.get_category_by_lang(category_name, "bo")
+    assert output == [
+        {
+            "name": "ཁ་འདོན།",
+            "heDesc": "ཁ་འདོན་འགྲེལ་བཤད་",
+            "heShortDesc": "ཁ་འདོན་འགྲེལ་བཤད་ཐུང་ཐུང་",
+        },
+        {
+            "name": "སྨོན་ལམ།",
+            "heDesc": "སྨོན་ལམ་འགྲེལ་བཤད་",
+            "heShortDesc": "སྨོན་ལམ་འགྲེལ་བཤད་ཐུང་ཐུང་",
+        },
+        {
+            "name": "བཟང་སྤྱོད་སྨོན་ལམ།",
+            "heDesc": "བཟང་སྤྱོད་འགྲེལ་བཤད་",
+            "heShortDesc": "བཟང་སྤྱོད་འགྲེལ་བཤད་ཐུང་ཐུང་",
+        },
+        {"name": "འགྲེལ་བ།", "heDesc": "", "heShortDesc": ""},
+        {"name": "བཟང་སྤྱོད་སྨོན་ལམ་འགྲེལ་བ་དེབ་། ༢", "heDesc": "", "heShortDesc": ""},
+    ]
+
+    category_name = "The Prayer of Good Actions Root Text Book"
+    output = categorizer.get_category_by_lang(category_name, "en")
+    assert output == [
+        {
+            "name": "Recitation",
+            "enDesc": "Explanation of Recitation",
+            "enShortDesc": "Brief Explanation of Recitation",
+        },
+        {
+            "name": "Aspiration Prayer",
+            "enDesc": "Explanation of Aspiration Prayer",
+            "enShortDesc": "Brief Explanation of Aspiration Prayer",
+        },
+        {"name": "The Prayer of Good Actions", "enDesc": "", "enShortDesc": ""},
+        {"name": "Root Text", "enDesc": "", "enShortDesc": ""},
+        {
+            "name": "The Prayer of Good Actions Root Text Book",
+            "enDesc": "",
+            "enShortDesc": "",
+        },
+    ]
+
 
 def test_get_category():
     DATA_DIR = Path(__file__).parent / "data"
