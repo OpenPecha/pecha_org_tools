a7972fc3d0d80d89483b4fe4c34464bfe493e1c2
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-12-20 15:56:17 +0530

    update test extract category

--- tests/extract/test_extract_category.py
+++ tests/extract/test_extract_category.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from pecha_org_tools.extract import CategoryExtractor
+from pecha_org_tools.utils import read_json
 
 
 def test_extract_category():
@@ -9,85 +10,12 @@ def test_extract_category():
 
     extractor = CategoryExtractor(input_xlsx)
     extractor.extract_categories()
-    assert extractor.bo_extracted_categories == [
-        ["ཁ་འདོན།(ཁ་འདོན་འགྲེལ་བཤད་)(ཁ་འདོན་འགྲེལ་བཤད་ཐུང་ཐུང་)"],
-        [
-            "ཁ་འདོན།(ཁ་འདོན་འགྲེལ་བཤད་)(ཁ་འདོན་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "སྨོན་ལམ།(སྨོན་ལམ་འགྲེལ་བཤད་)(སྨོན་ལམ་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-        ],
-        [
-            "ཁ་འདོན།(ཁ་འདོན་འགྲེལ་བཤད་)(ཁ་འདོན་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "སྨོན་ལམ།(སྨོན་ལམ་འགྲེལ་བཤད་)(སྨོན་ལམ་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "ཀུན་བཟང་སྨོན་ལམ།",
-        ],
-        [
-            "ཁ་འདོན།(ཁ་འདོན་འགྲེལ་བཤད་)(ཁ་འདོན་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "སྨོན་ལམ།(སྨོན་ལམ་འགྲེལ་བཤད་)(སྨོན་ལམ་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "ཀུན་བཟང་སྨོན་ལམ།",
-            "རྩ་བ།",
-        ],
-        [
-            "ཁ་འདོན།(ཁ་འདོན་འགྲེལ་བཤད་)(ཁ་འདོན་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "སྨོན་ལམ།(སྨོན་ལམ་འགྲེལ་བཤད་)(སྨོན་ལམ་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "ཀུན་བཟང་སྨོན་ལམ།",
-            "འགྲེལ་བ།",
-        ],
-        [
-            "ཁ་འདོན།(ཁ་འདོན་འགྲེལ་བཤད་)(ཁ་འདོན་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "སྨོན་ལམ།(སྨོན་ལམ་འགྲེལ་བཤད་)(སྨོན་ལམ་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "བཟང་སྤྱོད་སྨོན་ལམ།(བཟང་སྤྱོད་འགྲེལ་བཤད་)(བཟང་སྤྱོད་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-        ],
-        [
-            "ཁ་འདོན།(ཁ་འདོན་འགྲེལ་བཤད་)(ཁ་འདོན་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "སྨོན་ལམ།(སྨོན་ལམ་འགྲེལ་བཤད་)(སྨོན་ལམ་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "བཟང་སྤྱོད་སྨོན་ལམ།(བཟང་སྤྱོད་འགྲེལ་བཤད་)(བཟང་སྤྱོད་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "རྩ་བ།",
-        ],
-        [
-            "ཁ་འདོན།(ཁ་འདོན་འགྲེལ་བཤད་)(ཁ་འདོན་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "སྨོན་ལམ།(སྨོན་ལམ་འགྲེལ་བཤད་)(སྨོན་ལམ་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "བཟང་སྤྱོད་སྨོན་ལམ།(བཟང་སྤྱོད་འགྲེལ་བཤད་)(བཟང་སྤྱོད་འགྲེལ་བཤད་ཐུང་ཐུང་)",
-            "འགྲེལ་བ།",
-        ],
-    ]
-    assert extractor.en_extracted_categories == [
-        ["Recitation (Explanation of Recitation)(Brief Explanation of Recitation)"],
-        [
-            "Recitation (Explanation of Recitation)(Brief Explanation of Recitation)",
-            "Aspiration Prayer (Explanation of Aspiration Prayer)(Brief Explanation of Aspiration Prayer)",
-        ],
-        [
-            "Recitation (Explanation of Recitation)(Brief Explanation of Recitation)",
-            "Aspiration Prayer (Explanation of Aspiration Prayer)(Brief Explanation of Aspiration Prayer)",
-            "The Prayer of Good Actions",
-        ],
-        [
-            "Recitation (Explanation of Recitation)(Brief Explanation of Recitation)",
-            "Aspiration Prayer (Explanation of Aspiration Prayer)(Brief Explanation of Aspiration Prayer)",
-            "The Prayer of Good Actions",
-            "Root Text",
-        ],
-        [
-            "Recitation (Explanation of Recitation)(Brief Explanation of Recitation)",
-            "Aspiration Prayer (Explanation of Aspiration Prayer)(Brief Explanation of Aspiration Prayer)",
-            "The Prayer of Good Actions",
-            "Commentary Text",
-        ],
-        [
-            "Recitation (Explanation of Recitation)(Brief Explanation of Recitation)",
-            "Aspiration Prayer (Explanation of Aspiration Prayer)(Brief Explanation of Aspiration Prayer)",
-            "The Prayer of Good Conduct (Explanation of Good Conduct)(Brief Explanation of Good Conduct)",
-        ],
-        [
-            "Recitation (Explanation of Recitation)(Brief Explanation of Recitation)",
-            "Aspiration Prayer (Explanation of Aspiration Prayer)(Brief Explanation of Aspiration Prayer)",
-            "The Prayer of Good Conduct (Explanation of Good Conduct)(Brief Explanation of Good Conduct)",
-            "Root Text",
-        ],
-        [
-            "Recitation (Explanation of Recitation)(Brief Explanation of Recitation)",
-            "Aspiration Prayer (Explanation of Aspiration Prayer)(Brief Explanation of Aspiration Prayer)",
-            "The Prayer of Good Conduct (Explanation of Good Conduct)(Brief Explanation of Good Conduct)",
-            "Commentary Text",
-        ],
-    ]
+
+    expected_bo_categories = read_json(DATA_DIR / "expected_bo_categories.json")
+    expected_en_categories = read_json(DATA_DIR / "expected_en_categories.json")
+
+    assert extractor.bo_extracted_categories == expected_bo_categories
+    assert extractor.en_extracted_categories == expected_en_categories
+
+
+test_extract_category()
